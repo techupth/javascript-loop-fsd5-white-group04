c3269ec61a691fd75df35d95f43500d910fd21ea
Gravizz <peeke0rr@gmail.com>
2023-11-30 23:03:08 +0700

    feat(ex-5):complete exercise

--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,8 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
-
+let f = 1;
 // Start coding here
+for (let i of companyName) {
+  console.log(`Number ` + f + ` character is ` + i);
+  f++;
+}
